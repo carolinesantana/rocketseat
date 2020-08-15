3b4a7dddfd6f2cfc30ff72a1b96ec214a5b570c4
Caroline Santana <carolinesantana.s@hotmail.com>
2020-08-14 21:53:48 -0300

    final proffy

---
+++ src/database/fake.js
@@ -0,0 +1,29 @@
+//dados
+const proffys = [
+    {
+    name: "Carol",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "0000",
+    bio: "Hello",
+    subject: "Matemática",
+    cost: "40",
+    weekday: [
+        1
+    ],
+    time_from: [720],
+    time_to: [1220]
+    },
+
+    {
+        name: "Renan",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "0000",
+        bio: "Hello",
+        subject: "Matemática",
+        cost: "40",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+        },
+
+]
--- src/server.js
+++ src/server.js
@@ -1,92 +1,8 @@
-//dados
-const proffys = [
-    {
-    name: "Carol",
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-    whatsapp: "0000",
-    bio: "Hello",
-    subject: "Matemática",
-    cost: "40",
-    weekday: [
-        1
-    ],
-    time_from: [720],
-    time_to: [1220]
-    },
-
-    {
-        name: "Renan",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "0000",
-        bio: "Hello",
-        subject: "Matemática",
-        cost: "40",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220]
-        },
-
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",                        
-    "Educação física",
-    "Física",
-    "Geografia",                  
-    "História",
-    "Matemática", 
-    "Português",                    
-    "Química"                      
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira", 
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-]
-
-//funcionalidades
-function getSubject (subjectNumber) {
-    const arrayPosition = +subjectNumber - 1
-    return subjects[arrayPosition]
-}
-
-function pageLanding (req,res) {
-    return res.render("index.html")
-}
-
-function pageStudy (req,res) {
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses (req,res) {
-    const data = req.query
-
-    //se tiver dados
-    const isNotEmpty = Object.keys(data).length != 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        //adc a lista de proffys
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    //se nao, mostrar página
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 //servidor
 const express = require('express');
 const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
 
 //configurar nunjucks (templete enginee)
 const nunjucks = require('nunjucks')
@@ -98,10 +14,13 @@ nunjucks.configure('src/views', {
 
 //inicializandoo servidor
 server
+    //receber dados do req body
+    .use(express.urlencoded({ extended: true})) 
     //configurar arqs estaticos, css, js, images
     .use(express.static("public"))
     //config rotas da app
     .get('/', pageLanding)
     .get('/study', pageStudy)
     .get('/give-classes', pageGiveClasses)
+    .post('/save-classes', saveClasses)
     .listen(3300)
